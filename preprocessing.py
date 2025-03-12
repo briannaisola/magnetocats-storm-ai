cbf72c474541fe0167aaadcdb3272cfa7dd01525
ramanm262 <rm1277@unh.edu>
2025-03-12 17:07:30 -0400

    Finished glob and file loaders

--- preprocessing.py
+++ preprocessing.py
@@ -6,24 +6,36 @@ import tqdm
 
 def load_omni(omni_path):
     omni_df = []
-    for file_name in tdqm.tqdm(glob.glob(omni_path), desc='Loading OMNI data'):
+    for file_name in tqdm.tqdm(glob.glob(omni_path), desc='Loading OMNI data'):
         file = pd.read_csv(file_name, header=None)
         omni_df.append(file)
     omni_df = pd.concat(omni_df)
+    return omni_df
 
 def load_sat_density(sat_density_path):
     sat_density_df = []
-    for file_name in tdqm.tqdm(glob.glob(sat_density_path), desc='Loading CHAMP data'):
+    for file_name in tqdm.tqdm(glob.glob(sat_density_path), desc='Loading CHAMP data'):
         file = pd.read_csv(file_name, header=None)
         sat_density_df.append(file)
     sat_density_df = pd.concat(sat_density_df)
+    return sat_density_df
 
 def load_goes(goes_path):
     goes_df = []
-    for file_name in tdqm.tqdm(glob.glob(goes_path), desc='Loading GOES data'):
+    for file_name in tqdm.tqdm(glob.glob(goes_path), desc='Loading GOES data'):
         file = pd.read_csv(file_name, header=None)
         goes_df.append(file)
     goes_df = pd.concat(goes_df)
+    return goes_df
 
-def preprocess():
-    pass
+def preprocess(omni_path, goes_path, sat_density_path):
+    load_omni(omni_path)
+    load_sat_density(sat_density_path)
+    load_sat_density(goes_path)
+
+if __name__ == "__main__":
+    path_start = "C:\\Users\\Raman\\Downloads\\phase_1\\"
+    omni_path = path_start + "omni2\*.csv"
+    sat_density_path = path_start + "sat_density\*.csv"
+    goes_path = path_start + "goes\*.csv"
+    preprocess(omni_path, goes_path, sat_density_path)
