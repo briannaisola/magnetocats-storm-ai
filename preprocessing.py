cd2f302ffecfcc7c4e0311db784ed52086d2be5d
rm1277 <rm1277@unh.edu>
2025-04-09 16:36:34 -0400

    Nothing important day commit

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ import numpy as np
 import glob
 import tqdm
 from sklearn.model_selection import train_test_split
+# from hapiclient import hapi
+# from hapiclient  import hapitime2datetime
+from datetime import datetime
 
 
 
@@ -16,15 +19,18 @@ def load_omni(omni_path):
     omni_df.set_index('Timestamp', inplace=True)
     # TODO omni_df = omni_df[[features we want]]
 
+    print("Created OMNI dataframe")
     return omni_df
 
 def load_goes(goes_path):
     goes_df = []
     for file_name in tqdm.tqdm(glob.glob(goes_path), desc='Loading GOES data'):
-        file = pd.read_csv(file_name, header=0)
+        file = pd.read_csv(file_name, header=0, usecols=["Timestamp", "xrsa_flux", "xrsa_flag", "xrsa_flag_excluded"])
         goes_df.append(file)
     goes_df = pd.concat(goes_df)
+    print("Created GOES dataframe")
     return goes_df
+#xrsb_flux_electrons or xrsb_flux_observed???,
 
 def load_sat_density(sat_density_path):
     sat_density_df = []
@@ -32,8 +38,8 @@ def load_sat_density(sat_density_path):
         file = pd.read_csv(file_name, header=0)
         sat_density_df.append(file)
     sat_density_df = pd.concat(sat_density_df)
+    print("Created Sat Density dataframe")
     return sat_density_df
-
 
 
 def preprocess(omni_path, goes_path, sat_density_path):
@@ -64,14 +70,63 @@ def preprocess(omni_path, goes_path, sat_density_path):
     input_train = train.drop(champ_data.columns, axis=1)
     input_valid = valid.drop(champ_data.columns, axis=1)
     input_test = test.drop(champ_data.columns, axis=1)
+    
+    # set timestamps as index
+    all_data.set_index('Timestamp', inplace=True)
+    
+    # save dataset
+    print("Saving Data")
+    fname = 'merged_data'
+    all_data.to_pickle('data/'+fname+'.pkl')
 
     # TODO: Feature engineering: remove unneeded features and create new features if we want
 
     # TODO: Save all the input and target data to individual files, which will be loaded later by a training script
+    
+def write2df(data_dir, fname='omni', filler_map=None):
+    """
+    Read and combine csv data and pickle as pandas dataframe (df).
+    (Optional) apply filler map and replace bad values with NaNs
+    
+    """
+    all_files = glob.glob(data_dir)
+    
+    df = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
+    
+    if filler_map is not None:
+        # replace all filler values with NaNs
+        df = df.applymap(lambda x: np.nan if float(x) in filler_map else x)
+        
+    # set timestamps as index
+    df.set_index('Timestamp', inplace=True)
+    
+    # save dataset
+    df.to_pickle('data/'+fname+'.pkl')
 
 if __name__ == "__main__":
-    path_start = "C:\\Users\\Raman\\Downloads\\phase_1\\"
-    omni_path = path_start + "omni2\*.csv"
-    sat_density_path = path_start + "sat_density\*.csv"
-    goes_path = path_start + "goes\*.csv"
+    path_start = "../data/dataset/"
+    omni_path = path_start + "omni2/*.csv"
+    sat_density_path = path_start + "sat_density/*.csv"
+    goes_path = path_start + "goes/*.csv"
     preprocess(omni_path, goes_path, sat_density_path)
+    # load_goes(goes_path)
+    
+    
+    # get omni metadata
+    # server = "https://cdaweb.gsfc.nasa.gov/hapi" # hapi server url
+    # dataset = "OMNI_HRO_5MIN" # OMNI dataset
+    # meta = hapi(server, dataset) # pull meta 
+    
+    # omni_filler = []
+    # # loop over all omni paramters in meta (ignoring 'time') and collect unique fill values
+    # for meta_var in meta['parameters'][1:]:  
+    #     fill_val = meta_var['fill'] # get filler value (e.g. 9999.99)
+        
+    #     # check if exists and save float to list
+    #     if float(fill_val) not in omni_filler:
+    #         omni_filler.append(float(fill_val))
+            
+    # write data to pandas df files; save as .pkl
+    # write2df(omni_path, fname='omni', filler_map=None)
+    # write2df(sat_density_path, fname='sat', filler_map=None)
+    # write2df(goes_path, fname='goes', filler_map=None)
--- process_omni.py
+++ process_omni.py
@@ -1,5 +1,6 @@
 import csv
 import pickle
+import glob
 import numpy as np
 import pandas as pd
 from hapiclient import hapi
@@ -7,14 +8,17 @@ from hapiclient  import hapitime2datetime
 from datetime import datetime
 
 # dropbox_url = "https://www.dropbox.com/scl/fo/ilxkfy9yla0z2ea97tfqv/AB9lngJ2yHvf9t5h2oQXaDc?rlkey=iju8q5b1kxol78kbt0b9tcfz3&e=2&st=vzi32eq6&dl=0"
+path_start = "../data/dataset/"
+omni_dir = omni_path = path_start + "omni2/*.csv"
 
-data_dir = "/Users/bisola/dev/unh-srl/magnetocats-storm-ai/data/sample_data/"
-omni_fname = "omni2-00007-20000705_to_20000903.csv"
+# omni_fname = "omni2-00007-20000705_to_20000903.csv"
 
 if __name__ == "__main__":
     
     # read csv data and save to pandas dataframe (df)
-    df = pd.read_csv(data_dir+omni_fname)
+    all_files = glob.glob(omni_dir)
+    
+    df = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
     
     # get omni metadata
     server = "https://cdaweb.gsfc.nasa.gov/hapi" # hapi server url
@@ -41,4 +45,23 @@ if __name__ == "__main__":
 
     # check
     print(df.head(5))
+    
+    def write2df(data_dir, fname='omni', filler_map=None):
+        # read csv data and save to pandas dataframe (df)
+        all_files = glob.glob(data_dir)
+        
+        df = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
+        
+        if filler_map is not None:
+            # replace all filler values with NaNs
+            df = df.applymap(lambda x: np.nan if float(x) in filler_map else x)
+            
+        # set timestamps as index
+        df.set_index('Timestamp', inplace=True)
+        
+        # save dataset
+        df.to_pickle('data/'+fname+'.pkl')
+        
+            
+            
     
